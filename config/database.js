883a8184c14165aea6e2f6760fd0cbb30b7f701e
Sirikran Suwanpimol <kran@Sirikrans-MacBook-Pro.local>
2019-12-07 22:40:05 -0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  database: process.env.MONGODB_URI || 'mongodb://localhost:27017/mongoScraper'
+  database: process.env.MONGODB_URI || 'mongodb://username1:password1@ds253348.mlab.com:53348/heroku_klh7mwhk'
 };
